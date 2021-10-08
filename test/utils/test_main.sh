3cdacb0e2346dd336040b2de40fea1665968385c
Jinwook Jeong <vustthat@gmail.com>
2021-10-09 02:47:57 +0900

    remove "test artifact" feature

--- test/utils/test_main.sh
+++ test/utils/test_main.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
-set -eu
-shopt -s nullglob
-
-FLUSH_ARTIFACTS=
-case $1 in
-  -f) FLUSH_ARTIFACTS=.; shift
-esac
-
-test_artifact_dir="test/artifacts/$1"
-mkdir -p "$test_artifact_dir"
-if [ $FLUSH_ARTIFACTS ]; then
-  rm -f "$test_artifact_dir"/*
-fi
-
-ARTIFACT_DIR=$test_artifact_dir "test/${1}.sh" || true
+"test/${1}.sh"
